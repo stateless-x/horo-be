4938a2f88a53c97d09682a45a09ccf735bb02ac0
stateless-x <purin.buriwong@gmail.com>
2026-03-02 20:47:35 +0700

    improve mbti guidance

--- lib/shared/index.ts
+++ lib/shared/index.ts
@@ -8,6 +8,7 @@ export * from './types/compatibility';
 export * from './constants/thai-time';
 export * from './constants/design';
 export * from './constants/mbti';
+export * from './constants/mbti-guidance';
 
 // Utils
 export * from './utils/date';
